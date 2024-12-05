208095f828d1e2c963a087ce71d00ec3af628a2d
Tom Bancroft <tom.bancroft@three-ships.com>
2024-12-05 10:20:37 -0500

    demo timeline running at /tom

---
+++ app/tom/page.tsx
@@ -0,0 +1,6 @@
+import Timeline from "@/components/timeline";
+// import Image from "next/image";
+
+export default function JFDI() {
+  return <Timeline />;
+}
--- components/timeline.tsx
+++ components/timeline.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { FaBuilding, FaSchoolFlag, FaStar } from "react-icons/fa6";
+
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -15,7 +17,7 @@ export default function Timeline() {
         contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
         date="2011 - present"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<WorkIcon />}
+        icon={<FaBuilding />}
       >
         <h3 className="vertical-timeline-element-title">Creative Director</h3>
         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -28,7 +30,7 @@ export default function Timeline() {
         className="vertical-timeline-element--work"
         date="2010 - 2011"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<WorkIcon />}
+        icon={<FaBuilding />}
       >
         <h3 className="vertical-timeline-element-title">Art Director</h3>
         <h4 className="vertical-timeline-element-subtitle">
@@ -43,7 +45,7 @@ export default function Timeline() {
         className="vertical-timeline-element--work"
         date="2008 - 2010"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<WorkIcon />}
+        icon={<FaBuilding />}
       >
         <h3 className="vertical-timeline-element-title">Web Designer</h3>
         <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
@@ -53,7 +55,7 @@ export default function Timeline() {
         className="vertical-timeline-element--work"
         date="2006 - 2008"
         iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        icon={<WorkIcon />}
+        icon={<FaBuilding />}
       >
         <h3 className="vertical-timeline-element-title">Web Designer</h3>
         <h4 className="vertical-timeline-element-subtitle">
@@ -65,7 +67,7 @@ export default function Timeline() {
         className="vertical-timeline-element--education"
         date="April 2013"
         iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-        icon={<SchoolIcon />}
+        icon={<FaSchoolFlag />}
       >
         <h3 className="vertical-timeline-element-title">
           Content Marketing for Web, Mobile and Social Media
@@ -77,7 +79,7 @@ export default function Timeline() {
         className="vertical-timeline-element--education"
         date="November 2012"
         iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-        icon={<SchoolIcon />}
+        icon={<FaSchoolFlag />}
       >
         <h3 className="vertical-timeline-element-title">
           Agile Development Scrum Master
@@ -89,7 +91,7 @@ export default function Timeline() {
         className="vertical-timeline-element--education"
         date="2002 - 2006"
         iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-        icon={<SchoolIcon />}
+        icon={<FaSchoolFlag />}
       >
         <h3 className="vertical-timeline-element-title">
           Bachelor of Science in Interactive Digital Media Visual Imaging
@@ -99,7 +101,7 @@ export default function Timeline() {
       </VerticalTimelineElement>
       <VerticalTimelineElement
         iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
-        icon={<StarIcon />}
+        icon={<FaStar />}
       />
     </VerticalTimeline>
   );
