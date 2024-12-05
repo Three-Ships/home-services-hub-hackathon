297a929932f74233df90007ab38c3f157efdc29f
sheltonclinard <shelton.clinard@three-ships.com>
2024-12-05 11:40:28 -0500

    Header and title updates

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -3,13 +3,42 @@ import { RxAvatar } from 'react-icons/rx';
 export default function AppHeader() {
   return (
     <header className="sticky top-0 z-10 bg-black shadow-md">
-      <div className="flex items-center justify-between px-4 py-2">
-        <h1 className="text-xl font-bold">Cubby</h1>
-        <nav>
-          <ul className="flex gap-4">
-            <RxAvatar className="w-6 h-6" />
-          </ul>
-        </nav>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Cubby</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto" />
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi3eTr0cKcYtgr-WRkvJG-0iTLe-vqrsYtuw&s" />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </header>
   );
