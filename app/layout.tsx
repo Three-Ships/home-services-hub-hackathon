71846bf17d00a8279614ba9a492cf58f7a5e86ac
alex-3s <alex.munoz@three-ships.com>
2024-12-05 11:44:08 -0500

    updated theme, fixed form issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-black">
+    <html lang="en" className="bg-black" data-theme='forest'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
